ba01408845af69e337729f5433d622c46ce7f667
Carvalho <70244224+GuilhermeSC9@users.noreply.github.com>
2025-05-22 21:42:48 -0300

    Commit Inicial

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import WeatherPage from './pages/WeatherPage/WeatherPage'
+
+function App() {
+  return (
+   <div className="container">
+    <WeatherPage/>
+   </div> 
+  )}
+
+export default App
